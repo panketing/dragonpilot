52c8abc01f9a493c8471ee058d666db94c8861bb
dragonpilot <dgnpilot@gmail.com>
2019-09-05 10:18:55 +1000

    取消省電模式

--- panda/board/main.c
+++ panda/board/main.c
@@ -79,8 +79,8 @@ void started_interrupt_handler(uint8_t interrupt_line) {
     delay(100000);
 
     // set power savings mode here
-    int power_save_state = current_board->check_ignition() ? POWER_SAVE_STATUS_DISABLED : POWER_SAVE_STATUS_ENABLED;
-    set_power_save_state(power_save_state);
+    //int power_save_state = current_board->check_ignition() ? POWER_SAVE_STATUS_DISABLED : POWER_SAVE_STATUS_ENABLED;
+    set_power_save_state(POWER_SAVE_STATUS_DISABLED);
   }
   EXTI->PR = (1U << interrupt_line);
 }
